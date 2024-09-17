146035e8e7f37587465d75b583276f38a9fff59f
Aditya Gupta <rajaditya2399@gmail.com>
2024-09-18 00:21:13 +0530

    reactfrm

--- reactform/src/App.jsx
+++ reactform/src/App.jsx
@@ -22,8 +22,8 @@ function App() {
   //   setLastName(event.target.value)
 
   // }
-  const[formData , setFormData] = useState({
-          firstName: "", lastName: "",email: "", comments:"",isVisible:true
+  const [formData, setFormData] = useState({
+    firstName: "", lastName: "", email: "", comments: "", isVisible: true, mode: "", favCar: ""
   })
 
 
@@ -32,41 +32,83 @@ function App() {
   // console.log(formData.email)
   // console.log(formData.comments)
   console.log(formData)
-  function changeHandler(event){
-    const {name,value,checked,type} = event.target
-    setFormData(prevFormData=>{
-      return{
+  function changeHandler(event) {
+    const { name, value, checked, type, } = event.target
+    setFormData(prevFormData => {
+      return {
         ...prevFormData,
-        [name] : type === 'checkbox' ? checked : value
+        [name]: type === 'checkbox' ? checked : value
       }
     });
   }
+
+  function submitHandler(event){
+    event.preventDefault();
+    console.log('finally printing entire for ka data')
+    console.log(formData)
+  }
   return (
-   <div className='App'>
-    <form>
-      <input placeholder='first name' onChange={changeHandler} name='firstName' value={formData.firstName}>
-      </input>
-      <br></br>
-      <br></br>
+    <div className='App' >
+      <form onSubmit={submitHandler}>
+        <input placeholder='first name' onChange={changeHandler} name='firstName' value={formData.firstName}>
+        </input>
+        <br></br>
+        <br></br>
 
-      <input placeholder='last name' onChange={changeHandler} name='lastName' value={formData.lastName} >
-      </input>
-      <br></br>
-      <br></br>
+        <input placeholder='last name' onChange={changeHandler} name='lastName' value={formData.lastName} >
+        </input>
+        <br></br>
+        <br></br>
 
-      <input placeholder='email' onChange={changeHandler} name='email' value={formData.email} >
-      </input>
+        <input placeholder='email' onChange={changeHandler} name='email' value={formData.email} >
+        </input>
 
-      <br></br>
-      <br></br>
-      <textarea placeholder='enter your comments' onChange={changeHandler} name='comments' value={formData.comments}/ >
-      <br></br>
+        <br></br>
+        <br></br>
+        <textarea placeholder='enter your comments' onChange={changeHandler} name='comments' value={formData.comments} />
+        <br></br>
 
 
-      <input type='checkbox' onChange={changeHandler} name='isVisible' id='isVisible' checked={formData.isVisible}></input>
-      <label htmlFor='isVisible'>Am I Visible? </label>
-    </form>
-   </div>
+        <input type='checkbox' onChange={changeHandler} name='isVisible' id='isVisible' checked={formData.isVisible}></input>
+        <label htmlFor='isVisible'>Am I Visible? </label>
+        <br></br>
+        <br></br>
+
+
+        <fieldset>
+          <legend>MODE:</legend>
+
+          <input type='radio' onChange={changeHandler} name='mode' value="Online-Mode" id='Online-Mode'
+            checked={formData.mode === "Online-Mode"}>
+          </input>
+          <label htmlFor='Online-Mode'>Online Mode</label> {/* Corrected to 'Online-Mode' */}
+          <br />
+
+          <input type='radio' onChange={changeHandler} name='mode' value="Offline-Mode" id='Offline-Mode'
+            checked={formData.mode === "Offline-Mode"}>
+          </input>
+          <label htmlFor='Offline-Mode'>Offline Mode</label> {/* Corrected to 'Offline-Mode' */}
+          <br />
+        </fieldset>
+
+        <label htmlFor='favCar'>Tell me your Favorite Car</label>  &nbsp;
+
+        <select onChange={changeHandler} name='favCar' id='favCar' value={formData.favCar}>
+          <option value='scorpio'>Scorpio</option>
+          <option value='bolero'>Bolero</option>
+          <option value='thar'>Thar</option>
+          <option value='safari'>Safari</option>
+          <option value='endeavour'>Endeavour</option>
+          <option value='fortuner'>Fortuner</option>
+          <option value='land rover'>Land Rover</option>
+          <option value='range rover'>Range Rover</option>
+        </select>
+        <br></br>
+        <br></br>
+
+        <button>Submit</button>
+      </form>
+    </div>
   );
 }
 
